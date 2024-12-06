7475bd9223c81e8db03b3ca30bd580e5d6a843b8
robertrahardja <robertrahardja@gmail.com>
2024-12-06 14:43:02 +0800

    Added vite and front page

---
+++ app.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ExchangeTest from "./ExchangeTest";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<ExchangeTest />);
